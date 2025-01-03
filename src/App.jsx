d10c4e2c051d863f3e460e0481fdb13fef064a7e
Madina-Web-Developer <developermadina761@gmail.com>
2025-01-04 01:58:13 +0600

    add navbar

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import './App.css'
+import LayoutOne from './Components/LayoutOne/LayoutOne'
+import Home from './Components/Pages/Home'
+
+function App() {
+
+  const myRoute = createBrowserRouter(
+    createRoutesFromElements(
+
+     <Route>
+
+        <Route path='/' element={<LayoutOne/>}>
+
+        <Route index element={<Home/>}/>
+        
+        </Route>
+
+     </Route>
+    )
+  )
+
+
+  return (
+    <>
+      <RouterProvider router={myRoute}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Navbar.css'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/General Project.png'
+
+
+const Navbar = () => {
+  return (
+   <>
+
+   <div className="mina_all_nav">
+    <div className="container">
+
+        <div className="nav_mina">
+
+            <div className="logo">
+                <img src="" alt="" />
+            </div>
+
+            <div className="nav_text_part">
+                <ul>
+                    <li><Link></Link></li>
+                </ul>
+            </div>
+
+        </div>
+
+    </div>
+   </div>
+   
+   
+   </>
+  )
+}
+
+export default Navbar
